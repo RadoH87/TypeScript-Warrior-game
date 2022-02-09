41de7685585fadeb70f5eb0686b944a204046c7d
Radek Holubowski <radekh8787@gmail.com>
2022-02-09 17:52:56 +0000

    add types to arena file

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { Warrior } from "./libs/warrior";
 const fighter1 = new Warrior("Baba Yaga", 9, 120);
 const fighter2 = new Warrior("Yanosik", 7, 140);
 const arena = new Arena(fighter1, fighter2);
-let winner;
+let winner: Warrior | null;
 do {
   winner = arena.fight();
 } while (winner === null);
--- libs/arena.ts
+++ libs/arena.ts
@@ -1,7 +1,13 @@
 import { Warrior } from "./warrior";
 
+enum ActiveWarrior {
+  First,
+  Second,
+}
 export class Arena {
-  constructor(warrior1, warrior2) {
+  activeWarrior: ActiveWarrior = ActiveWarrior.Second;
+
+  constructor(public warrior1: Warrior, public warrior2: Warrior) {
     if (!(warrior1 instanceof Warrior)) {
       throw new Error("warrior1 must b an instance of Warrior class!");
     }
@@ -10,12 +16,16 @@ export class Arena {
     }
     this.warrior1 = warrior1;
     this.warrior2 = warrior2;
-    this.activeWarrior = 1;
   }
-
-  fight() {
-    const attacker = this.activeWarrior === 1 ? this.warrior1 : this.warrior2;
-    const attacked = this.activeWarrior === 1 ? this.warrior2 : this.warrior1;
+  fight(): Warrior | null {
+    const attacker =
+      this.activeWarrior === ActiveWarrior.First
+        ? this.warrior1
+        : this.warrior2;
+    const attacked =
+      this.activeWarrior === ActiveWarrior.First
+        ? this.warrior2
+        : this.warrior1;
 
     const attackingHitPoints = attacker.getHitPoints();
     const attackedOldHp = attacked.getHp();
@@ -29,11 +39,12 @@ export class Arena {
       attackedNewHp,
       "hp"
     );
-
     attacked.setHp(attackedNewHp);
 
-    this.activeWarrior = this.activeWarrior === 1 ? 2 : 1;
-
+    this.activeWarrior =
+      this.activeWarrior === ActiveWarrior.First
+        ? ActiveWarrior.Second
+        : ActiveWarrior.First;
     if (attackedNewHp <= 0) {
       console.log(attacked.getName(), " goes to Valhalla");
       return attacker;
